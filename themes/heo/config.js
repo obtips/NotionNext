fc1cb28d03c44ae578658911786020a425018eb6
ObsidianTips <166277238+obtips@users.noreply.github.com>
2025-10-23 17:10:51 +0800

    Update hero titles in config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '爱旅游',
-  HEO_HERO_TITLE_2: 'LuvlyChina.com',
-  HEO_HERO_TITLE_3: '山西古建筑',
+  HEO_HERO_TITLE_1: '山西古建专版',
+  HEO_HERO_TITLE_2: '爱旅游',
+  HEO_HERO_TITLE_3: 'LuvlyChina',
   HEO_HERO_TITLE_4: '震撼人心',
   HEO_HERO_TITLE_5: '最全最细致的山西旅游导览',
   HEO_HERO_TITLE_LINK: 'https://luvlychina.com',
